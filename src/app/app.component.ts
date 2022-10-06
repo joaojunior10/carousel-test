9548dcae7383f3abba2c7d01cde7d8853a403167
João Francisco <joao.francisco@tst.jus.br>
2022-10-06 19:05:07 -0300

    Create card carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Splide from '@splidejs/splide';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'carousel-test';
+
+  ngOnInit() {
+    const splide = new Splide('.splide', {
+      type: 'loop',
+      drag: 'free',
+      focus: 0,
+      perPage: 3
+    });
+    splide.mount();
+  }
 }
